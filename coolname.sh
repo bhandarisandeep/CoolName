01542cadbe8e2d8263f1aced48b5e8e241d8521a
bhandarisandeep <sandeepbhandari033@gmail.com>
2020-04-15 23:38:58 +0530

    added doh fixo

--- coolname.sh
+++ coolname.sh
@@ -3,6 +3,7 @@ PKG_OK=$(dpkg-query -W --showformat='${Status}\n' figlet|grep "install ok instal
 PKG1_OK=$(dpkg-query -W --showformat='${Status}\n' lolcat|grep "install ok installed")
 PKG2_OK=$(dpkg-query -W --showformat='${Status}\n' ruby|grep "install ok installed")
 checklol=$(command -v lolcat)
+checkdoh=$(ls /usr/share/figlet/doh.flf)
 echo Checking for Lolcat: $PKG1_OK
 echo Checking for Ruby: $PKG2_OK
 echo Checking for Figlet: $PKG_OK
@@ -10,8 +11,14 @@ if [ "" == "$PKG_OK" ];
 then
   echo "No cool figlet found I am installing for you!!!"
   sudo apt-get --force-yes --yes install figlet
+ if [ "" == "$checkdoh" ];
+ then
+  {
+  echo "No dog font found I am adding for you!!!"
   wget ftp://ftp.figlet.org/pub/figlet/fonts/contributed/doh.flf -P /usr/share/figlet/
-fi
+  }
+ fi
+fi  
 if [ "" == "$PKG2_OK" ]; 
 then
   echo "No cool Ruby found I am installing for you!!!"
