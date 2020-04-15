a054a9f9675084fa96b18b73c34db80014f1c9fe
bhandarisandeep <sandeepbhandari033@gmail.com>
2020-04-15 23:17:49 +0530

    added custom locat check

--- coolname.sh
+++ coolname.sh
@@ -2,22 +2,31 @@
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' figlet|grep "install ok installed")
 PKG1_OK=$(dpkg-query -W --showformat='${Status}\n' lolcat|grep "install ok installed")
 PKG2_OK=$(dpkg-query -W --showformat='${Status}\n' ruby|grep "install ok installed")
+checklol=$(command -v lolcat)
 echo Checking for Lolcat: $PKG1_OK
+echo Checking for Ruby: $PKG2_OK
 echo Checking for Figlet: $PKG_OK
 if [ "" == "$PKG_OK" ]; 
 then
   echo "No cool figlet found I am installing for you!!!"
   sudo apt-get --force-yes --yes install figlet
   wget ftp://ftp.figlet.org/pub/figlet/fonts/contributed/doh.flf /usr/share/figlet
-elif [ "" == "$PKG2_OK" ]; 
+fi
+if [ "" == "$PKG2_OK" ]; 
 then
   echo "No cool Ruby found I am installing for you!!!"
   sudo apt-get install ruby
-elif [ "" == "$PKG1_OK" ]; 
+fi
+if [ "" == "$PKG1_OK" ]; 
 then
+ if [ "" == "$checklol" ];
+ then
+  {
   echo "No cool lolcat found I am installing for you!!!"
   sudo gem install lolcat
-else
+  }
+ fi
+fi
 figlet -t "!!! Today we gonna print some cool name !!!"
 
 read -p 'Enter your Cool Name:' name
